74d6666f4343814fcc6167ec654ea00f31460672
Muhammad Umair <umaair40@gmail.com>
2025-09-18 17:54:15 +0500

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
   const audioRef = useRef();
   const seekBg = useRef();
   const seekBar = useRef();
-  const url='http://localhost:4000'
+  const url = import.meta.env.VITE_API_URL;
   const [songsData,setSongsData]=useState([]);
   const [albumsData,setAlbumsData]=useState([]);
   const [track, setTrack] = useState(songsData[0]);
